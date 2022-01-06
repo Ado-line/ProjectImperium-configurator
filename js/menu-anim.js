fa4e31cff02156d67e0d82daada14f7467ea65b7
Ado-line <adoline.mccammon@gmail.com>
2022-01-06 00:31:56 -0700

    Javascript Sucks, menu is working though!

--- js/menu-anim.js
+++ js/menu-anim.js
@@ -1,16 +1,44 @@
-document.getElementById('menu-bar').addEventListener("onmouseenter", MouseCheck("onmouseenter"))
-document.getElementById('menu-bar').addEventListener("onmouseleave", MouseCheck("onmouseleave"))
+
+var menuBar = document.getElementById('menu-bar');
+var navButtonTextHome = document.getElementById('nav-button-text-home');
+
+menuBar.addEventListener("mouseenter", MouseCheckEnter);
+menuBar.addEventListener("mouseleave", MouseCheckLeave);
 // document.getElementById('menu-bar').onmouseenter = MouseCheck("onmouseenter")
 // document.getElementById('menu-bar').onmouseleave = MouseCheck("onmouseleave")
 
-function MouseCheck(MouseAction) {
-  if (MouseAction = "onmouseenter") {
-    document.getElementById('menu-bar').style.width = '15%';
-    document.getElementById('menu-bar').style['box-shadow'] = '10px 0px 10px rgba(0,0,0,0.5)';
-  } else if (MouseAction = "onmouseleave") {
-    document.getElementById('menu-bar').style.width = '6%';
-    document.getElementById('menu-bar').style['box-shadow'] = '0px 0px 0px rgba(0,0,0,0)';
-} else {
-  console.log("Error: Bad Parameter")
+
+//  function MouseCheck(MouseAction) {
+//    console.log("MouseCheck")
+//    if (MouseAction = "onmouseenter") {
+//      document.getElementById('menu-bar').style.width = '15%';
+//      document.getElementById('menu-bar').style['box-shadow'] = '10px 0px 10px rgba(0,0,0,0.5)';
+//    } else if (MouseAction = "onmouseleave") {
+//      document.getElementById('menu-bar').style.width = '6%';
+//      document.getElementById('menu-bar').style['box-shadow'] = '0px 0px 0px rgba(0,0,0,0)';
+//    } else if (MouseAction !=("onmoseenter" || "onmouseleave")) {
+//    console.log("Error: Bad Parameter")
+//  }
+//  }
+
+//  MouseCheck("onmouseleave");
+
+
+function MouseCheckEnter() {
+  // console.log("Beeeeeg");
+  menuBar.style.width = '15%';
+  menuBar.style['box-shadow'] = '10px 0px 10px rgba(0,0,0,0.5)';
+  navButtonTextHome.style['clip-path'] = 'polygon(0% 0%, 0% 100%, 100% 100%, 100% 0%)';
 }
+
+function MouseCheckLeave() {
+  // console.log("Smoooool");
+  menuBar.style.width = '6%';
+  menuBar.style['box-shadow'] = '0px 0px 0px rgba(0,0,0,0)';
+  navButtonTextHome.style['clip-path'] = 'polygon(0% 0%, 0% 100%, 0% 100%, 0% 0%)';
+}
+
+function initAttention() {
+  MouseCheckEnter();
+  setTimeout(MouseCheckLeave, 1750);
 }
