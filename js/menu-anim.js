ccba239a1775e62f309652ac3e57dd81aff50c1d
Ado-line <adoline.mccammon@gmail.com>
2022-01-08 06:20:06 -0700

    Finally got a bunch of the menu animations working

--- js/menu-anim.js
+++ js/menu-anim.js
@@ -1,40 +1,27 @@
 
 var menuBar = document.getElementById('menu-bar');
 var navButtonTextHome = document.getElementById('nav-button-text-home');
+var mainContent = document.getElementById('main-content');
+var homeButton = document.getElementById('home-button');
 
 menuBar.addEventListener("mouseenter", MouseCheckEnter);
 menuBar.addEventListener("mouseleave", MouseCheckLeave);
-// document.getElementById('menu-bar').onmouseenter = MouseCheck("onmouseenter")
-// document.getElementById('menu-bar').onmouseleave = MouseCheck("onmouseleave")
-
-
-//  function MouseCheck(MouseAction) {
-//    console.log("MouseCheck")
-//    if (MouseAction = "onmouseenter") {
-//      document.getElementById('menu-bar').style.width = '15%';
-//      document.getElementById('menu-bar').style['box-shadow'] = '10px 0px 10px rgba(0,0,0,0.5)';
-//    } else if (MouseAction = "onmouseleave") {
-//      document.getElementById('menu-bar').style.width = '6%';
-//      document.getElementById('menu-bar').style['box-shadow'] = '0px 0px 0px rgba(0,0,0,0)';
-//    } else if (MouseAction !=("onmoseenter" || "onmouseleave")) {
-//    console.log("Error: Bad Parameter")
-//  }
-//  }
-
-//  MouseCheck("onmouseleave");
-
 
 function MouseCheckEnter() {
   // console.log("Beeeeeg");
+  homeButton.style.width = '15vw';
   menuBar.style.width = '15%';
   menuBar.style['box-shadow'] = '10px 0px 10px rgba(0,0,0,0.5)';
+  mainContent.style['padding-left'] = '16%';
   navButtonTextHome.style['clip-path'] = 'polygon(0% 0%, 0% 100%, 100% 100%, 100% 0%)';
 }
 
 function MouseCheckLeave() {
   // console.log("Smoooool");
+  setTimeout(function(){homeButton.style.width = '6vw';}, 500);
   menuBar.style.width = '6%';
   menuBar.style['box-shadow'] = '0px 0px 0px rgba(0,0,0,0)';
+  mainContent.style['padding-left'] = '7%';
   navButtonTextHome.style['clip-path'] = 'polygon(0% 0%, 0% 100%, 0% 100%, 0% 0%)';
 }
 
