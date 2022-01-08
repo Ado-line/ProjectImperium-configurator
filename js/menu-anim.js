a7e9ad2e013774613191bc95161ae81f12025d06
Ado-line <adoline.mccammon@gmail.com>
2022-01-08 07:43:20 -0700

    Menu animations & main content div work now, everything's slippy slide-y

--- js/menu-anim.js
+++ js/menu-anim.js
@@ -1,7 +1,7 @@
 
 var menuBar = document.getElementById('menu-bar');
 var navButtonTextHome = document.getElementById('nav-button-text-home');
-var mainContent = document.getElementById('main-content');
+var mainContent1 = document.getElementById('main-content-1');
 var homeButton = document.getElementById('home-button');
 
 menuBar.addEventListener("mouseenter", MouseCheckEnter);
@@ -12,8 +12,9 @@ function MouseCheckEnter() {
   homeButton.style.width = '15vw';
   menuBar.style.width = '15%';
   menuBar.style['box-shadow'] = '10px 0px 10px rgba(0,0,0,0.5)';
-  mainContent.style['padding-left'] = '16%';
+  mainContent1.style['padding-left'] = '16%';
   navButtonTextHome.style['clip-path'] = 'polygon(0% 0%, 0% 100%, 100% 100%, 100% 0%)';
+  setTimeout(function(){homeButton.style.width = '15vw';}, 500);
 }
 
 function MouseCheckLeave() {
@@ -21,7 +22,7 @@ function MouseCheckLeave() {
   setTimeout(function(){homeButton.style.width = '6vw';}, 500);
   menuBar.style.width = '6%';
   menuBar.style['box-shadow'] = '0px 0px 0px rgba(0,0,0,0)';
-  mainContent.style['padding-left'] = '7%';
+  mainContent1.style['padding-left'] = '7%';
   navButtonTextHome.style['clip-path'] = 'polygon(0% 0%, 0% 100%, 0% 100%, 0% 0%)';
 }
 
